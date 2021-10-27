ffd8f8b26ff8ae71e7a904251de7a8595f765f6e
Dmytro Kolesnyk <ds.kolesnyk@gmail.com>
2021-10-27 15:11:44 +0300

    Lesson 72

---
+++ Lesson72_Keyword Var/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson72_Keyword_Var
+{
+    // Ключевое слово Var
+    // Неявно типизированные локальные переменные
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
