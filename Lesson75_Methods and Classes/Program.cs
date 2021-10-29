9dc881b943ed93e0b5ab495854fcb87a61fdaa8f
Dmytro Kolesnyk <ds.kolesnyk@gmail.com>
2021-10-29 14:44:24 +0300

    Lesson 75

---
+++ Lesson75_Methods and Classes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson75_Methods_and_Classes
+{
+    class Program
+    {
+        static Student GetStudent()
+        {
+            Student student = new Student();
+            student.lastName = "Shevchenko";
+            student.firstName = "Ivan";
+            student.middleName = "Sergiyovych";
+            student.id = Guid.NewGuid();
+            student.age = 17;
+            student.group = "3DID";
+            return student;
+        }
+
+        static void Main(string[] args)
+        {
+            var firstStudent = GetStudent();
+            firstStudent.Print();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var secondStudent = new Student();
+            secondStudent.firstName = "No name";
+            secondStudent.Print();
+        }
+    }
+}
---
+++ Lesson75_Methods and Classes/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson75_Methods_and_Classes
+{
+    class Student
+    {
+        public Guid id;
+        public string firstName;
+        public string lastName;
+        public string middleName;
+        public int age;
+        public string group;
+
+        public void Print()
+        {
+            Console.WriteLine("Информация о студенте");
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"First name: {firstName}");
+            Console.WriteLine($"Last name: {lastName}");
+            Console.WriteLine($"Middle name: {middleName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Group: {group}");
+        }
+    }
+}
