a0bc20c7f7059be084f150a9808a31c8c75fd4b1
Dmytro Kolesnyk <ds.kolesnyk@gmail.com>
2021-10-31 13:56:05 +0200

    Lesson 78

--- Lesson78_Class Constructor/Program.cs
+++ Lesson78_Class Constructor/Program.cs
@@ -7,10 +7,45 @@ using System.Threading.Tasks;
 namespace Lesson78_Class_Constructor
 {
     // Конструктор класса. Конструктор по умолчанию
+    class Gun
+    {
+        public Gun(bool isLoaded)    // Конструктор класса. В скобках принимаемые параметры, не обязательно
+        {
+
+            _isLoaded = isLoaded;
+            //this.isLoaded = isLoaded;
+            //Reload();
+            // isLoaded = true;
+        }
+
+        private bool _isLoaded;
+
+        private void Reload()
+        { 
+            Console.WriteLine("Заряжаю");
+            _isLoaded = true;
+            Console.WriteLine("Заряжено");
+        }
+        public void Shoot()
+        {
+            if (!_isLoaded) 
+            {
+                Console.WriteLine("Оружие не заряжено");
+                Reload();
+            }
+            Console.WriteLine("Стреляю, стреляю ...");
+            _isLoaded = false;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Gun gun = new Gun(false);   // Gun() - конструктор класса по умолчанию. Реализовано так, что
+                                        // мы имеем возможность создавать пушку в 2 вариантах, заряженую либо разряженую изначально
+            gun.Shoot();
+            //gun.Shoot();
+
         }
     }
 }
---
+++ Lesson78_Class Constructor_Example Point/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson78_Class_Constructor_Example_Point
+{
+    class Point
+    {
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+        private int _x;
+        private int _y;
+
+        public void Print()
+        {
+            Console.WriteLine($"X: {_x} | Y: {_y}");
+        }
+      
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point(x: 5, y: 3);
+            point.Print();
+        }
+    }
+}
