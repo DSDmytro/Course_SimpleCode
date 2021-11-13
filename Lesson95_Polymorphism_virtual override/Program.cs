86a4771151f4195ae2fa86360728786e492dab19
Dmytro Kolesnyk <ds.kolesnyk@gmail.com>
2021-11-13 13:42:07 +0200

    Lesson 95

---
+++ Lesson95_Polymorphism_virtual override/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson95_Polymorphism_virtual_override
+{
+    // Полиморфизм
+    // виртуальные методы
+    // virtual override 
+    class Car
+    {
+        protected void StartEngine()
+        {
+            Console.WriteLine("Запускаем двигатель");
+        }
+        public virtual void Drive()
+        {
+            StartEngine();
+            Console.WriteLine("Еду");
+        }
+    }
+    class Sportcar: Car
+    {
+        public override void Drive()
+        {
+            StartEngine();
+            // base.Drive();
+            Console.WriteLine("Еду очень быстро");
+        }
+    }
+    class Person // ключевой момент, что при этом мы не меняем класс Person
+    {
+        public void Drive(Car car)
+        {
+            car.Drive();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Car car = new Car();
+            Person person = new Person();
+            person.Drive(new Car());
+            person.Drive(new Sportcar());
+        }
+    }
+}
