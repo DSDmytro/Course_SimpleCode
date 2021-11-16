715264c144ca0fc6a9223eabd7f722cb2031b651
Dmytro Kolesnyk <ds.kolesnyk@gmail.com>
2021-11-16 21:44:10 +0200

    Lesson 99

---
+++ Lesson98_Interface Inheritance/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson98_Interface_Inheritance
+{
+    // Наследование интерфейсов 
+    interface IWeapon
+    {
+        void Fire();
+    }
+    interface IThrowWeapon : IWeapon
+    {
+        void Throw();
+    }
+    class Gun : IWeapon
+    {
+        public void Fire()
+        {
+            Console.WriteLine($"{GetType().Name} : Пушка стреляет!");
+        }
+    }
+    class LaserGun : IWeapon
+    {
+        public void Fire()
+        {
+            Console.WriteLine($"{GetType().Name} : Лазер стреляет!");
+        }
+    }
+    class Knife : IThrowWeapon
+    {
+        public void Fire()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine($"{GetType().Name} : Режу ножом");
+        }
+
+        public void Throw()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine($"{GetType().Name} : Метаю нож");
+        }
+    }
+    class Player
+    {
+        public void Fire(IWeapon weapon)
+        {
+            weapon.Fire();
+        }
+        public void Throw(IThrowWeapon weapon)
+        {
+            weapon.Throw();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            IWeapon[] inventory = { new Gun(), new LaserGun(), new Knife() };
+            foreach (var item in inventory)
+            {
+                player.Fire(item);
+                Console.WriteLine();
+            }
+            player.Throw(new Knife());
+        }
+    }
+}
---
+++ Lesson99_Explicit interface implementation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson99_Explicit_Interface_Implementation
+{
+    // Явная реализация интерфейса
+    interface IFirstInterface
+    {
+        void Action();
+    }
+    interface ISecondInterface
+    {
+        void Action();
+    }
+    class MyClass : IFirstInterface, ISecondInterface
+    {
+        public void Action()
+        {
+            Console.WriteLine("My Class Action...");
+        }
+    }
+    class MyOtherClass : IFirstInterface, ISecondInterface
+    {
+        void IFirstInterface.Action() // явная реализация интерфейса. Не может быть public
+        {
+            Console.WriteLine("My Other Class, First Interfase Action");        
+        }
+        void ISecondInterface.Action() // явная реализация интерфейса
+        {
+            Console.WriteLine("My Other Class, Second Interfase Action");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            Foo(myClass);
+            Bar(myClass);
+            Console.WriteLine();
+            MyOtherClass myOtherClass = new MyOtherClass();
+            Foo(myOtherClass);
+            Bar(myOtherClass);
+        }
+        static void Foo(IFirstInterface firstInterface)
+        {
+            firstInterface.Action();
+        }
+        static void Bar(ISecondInterface secondInterface)
+        {
+            secondInterface.Action();
+        }
+    }
+}
