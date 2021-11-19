f1a2ecaa2e563bf52a0eb72ea1d9a207f9657fa9
Dmytro Kolesnyk <ds.kolesnyk@gmail.com>
2021-11-19 21:16:05 +0200

    Lesson 102

---
+++ Lesson102_Boxing, Unboxing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson102_Boxing__Unboxing
+{
+    // Boxing/Unboxing
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;           // int - структура, хранится в стеке 
+            object b = a;        // выполняется операция упаковки, object - базовый класс для всех типов данных, размещается в куче
+            int c = (int )b;     // оперяция распаковки, выглядит как приведение типов
+            // decimal d = (decimal)b;  - ошибка, распаковать можно только в int
+            a.GetType(); // Упаовка. а - типа int. GetType метод вызывается у типа Object 
+        }
+    }
+}
